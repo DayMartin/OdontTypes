aa7a5468f97cb9121452341ec1518b1f97ca5d30
DayMartin <negociosdinah@gmail.com>
2024-05-06 14:20:43 -0300

    Feat: Server para conexão do todo

--- back/src/Server/Server.ts
+++ back/src/Server/Server.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import { router } from './routes/router';
+import 'dotenv/config';
+import queryDatabase from "../Server/database/queryPromise"
+import { CreateTables } from './database/conn';
 
 const server = express();
 
-server.get("/", (_, res) => {
-    return res.send('Olá dev!');
-});
+server.use(express.json());
+server.use(router);
+
+// Create tables
+
+// Crie uma instância da classe ou objeto
+const dbManager = new CreateTables();
+// Chame o método createUsuariosTable()
+dbManager.createAllTables();
+
+// Routes
+server.use("/api", router);
 
 export { server };
