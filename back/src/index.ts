487e9d935bcaf11c4eceffed18d892f48c78ce0a
DayMartin <negociosdinah@gmail.com>
2024-05-06 14:22:06 -0300

    feat: Index centralizado

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,6 +1,9 @@
 import { server } from "./Server/Server";
+import { CreateTables } from "../src/Server/database/conn"
 
-const port = 3048;
+const port = process.env.PORT
+;
+
 
 server.listen(port, () => {
     console.log(`Servidor rodando da porta ${port}`)
