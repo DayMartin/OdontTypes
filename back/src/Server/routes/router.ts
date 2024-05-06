201b39cf000e99a857fdba2d43548cddbe223315
DayMartin <negociosdinah@gmail.com>
2024-05-06 14:20:17 -0300

    feat: Adicionado rotas

---
+++ back/src/Server/routes/router.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { usersRouter } from "./users";
+// import servicoRouter from "./servicos";
+// import osRouter from "./os";
+// import parcelasRouter from "./parcelas";
+
+const router = Router();
+
+router.use("/", usersRouter);
+// router.use("/", servicoRouter);
+// router.use("/", osRouter);
+// router.use("/", parcelasRouter);
+
+export { router };
