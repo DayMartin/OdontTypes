c26abb3b5b788f8e7ced5463e0121f1e09e40fb8
DayMartin <negociosdinah@gmail.com>
2024-05-07 15:32:49 -0300

    feat: test de integração com todas as rotas de Users

--- back/tests/Users/get.test.ts
+++ back/tests/Users/get.test.ts
@@ -44,6 +44,42 @@ describe('Users - Consulta', () => {
 				  "senha": "giovana0407",
 				  "status": "Ativo",
 				  "data_criacao": "2024-05-06T18:32:45.000Z"
+				},
+				{
+				  "id": 5,
+				  "tipo": null,
+				  "cpfcnpj": null,
+				  "nome": null,
+				  "telefone": null,
+				  "endereco": null,
+				  "email": null,
+				  "senha": null,
+				  "status": null,
+				  "data_criacao": "2024-05-07T17:53:47.000Z"
+				},
+				{
+				  "id": 6,
+				  "tipo": null,
+				  "cpfcnpj": null,
+				  "nome": null,
+				  "telefone": null,
+				  "endereco": null,
+				  "email": null,
+				  "senha": null,
+				  "status": null,
+				  "data_criacao": "2024-05-07T17:54:18.000Z"
+				},
+				{
+				  "id": 7,
+				  "tipo": "paciente",
+				  "cpfcnpj": "12345678900",
+				  "nome": "Doria",
+				  "telefone": "(99) 991745323",
+				  "endereco": "Rua dr jose maria whitaker",
+				  "email": "paciente5@gmail.com",
+				  "senha": "giovana0407",
+				  "status": "Ativo",
+				  "data_criacao": "2024-05-07T18:01:00.000Z"
 				}
 			  ]
 		)
