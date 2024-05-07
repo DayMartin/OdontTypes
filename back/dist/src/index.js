341a240167dab6041001a09b66f09fe6b5976ea5
DayMartin <negociosdinah@gmail.com>
2024-05-07 11:41:13 -0300

    feat: config jest

--- back/dist/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Server_1 = require("./Server/Server");
-const port = 3048;
-Server_1.server.listen(port, () => {
-    console.log(`Servidor rodando da porta ${port}`);
-});
-//# sourceMappingURL=index.js.map
