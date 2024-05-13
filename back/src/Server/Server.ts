d7a8b4a6282b107eb44d0569a885604a27ebd2ce
DayMartin <negociosdinah@gmail.com>
2024-05-13 02:49:39 -0300

    feat: incluido cors

--- back/src/Server/Server.ts
+++ back/src/Server/Server.ts
@@ -2,11 +2,15 @@ import express from 'express';
 import { router } from './routes/router';
 import 'dotenv/config';
 import { connectToDatabase } from './database/connection';
+import cors from "cors";
 
 const server = express();
 
 server.use(express.json());
 server.use(router);
+// Configurando o middleware CORS
+server.use(cors());
+
 
 //DB conect
 connectToDatabase()
